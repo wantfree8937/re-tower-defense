e2355f006ce06ab04a9e5ca116800d3b2ec817db
uchanjeon <uchanjeon@gmail.com>
2024-07-24 16:58:53 +0900

    중간 점검

--- src/handlers/game/tower.handler.js
+++ src/handlers/game/tower.handler.js
@@ -1,4 +1,4 @@
-// import { getUsers } from "../models/user.model.js";
+import { getUsers } from '../models/user.model.js';
 import { getTowers } from '../../models/tower.model.js';
 
 // 타워 정보 저장
@@ -53,22 +53,22 @@ export const saveTowerInfoHandler = (uuid, payload) => {
 //   };
 // };
 
-// export const upgradeTower = (uuid, payload) => {
-//   const towers = getTowers();
-//   const { towerIndex } = payload;
+export const upgradeTower = (uuid, payload) => {
+  const towers = getTowers();
+  const { towerIndex } = payload;
 
-//   const userId = getUsers();
+  const userId = getUsers();
 
-//   if (userId[0].uuid !== uuid) {
-//     return { status: 'fail', message: '유저 정보가 다릅니다.' };
-//   }
+  if (userId[0].uuid !== uuid) {
+    return { status: 'fail', message: '유저 정보가 다릅니다.' };
+  }
 
-//   if (!towers[towerIndex]) {
-//     return { status: 'fail', message: '서버에 존재하지 않는 대상입니다.' };
-//   }
+  if (!towers[towerIndex]) {
+    return { status: 'fail', message: '서버에 존재하지 않는 대상입니다.' };
+  }
 
-//   return {
-//     status: 'success',
-//     message: '대상 확인, 타워를 업그레이드했습니다.',
-//   };
-// };
+  return {
+    status: 'success',
+    message: '대상 확인, 타워를 업그레이드했습니다.',
+  };
+};
