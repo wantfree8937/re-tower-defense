5f0551a68f0eae905c219d4b5db63cd8690af0e7
uchanjeon <uchanjeon@gmail.com>
2024-07-24 17:00:59 +0900

    중간 저장

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,7 +1,6 @@
 import { gameEnd, gameStart } from './game/game.handler.js';
-import { monsterCreateHandler, monsterKillHandler } from './game/monster.handler.js'
-// import { createTower, upgradeTower } from './tower.handler.js'
-import { saveTowerInfoHandler } from './game/tower.handler.js'
+import { monsterCreateHandler, monsterKillHandler } from './game/monster.handler.js';
+import { saveTowerInfoHandler, upgradeTower } from './game/tower.handler.js';
 
 const handlerMappings = {
   2: gameStart,
@@ -9,9 +8,8 @@ const handlerMappings = {
   11: monsterCreateHandler,
   12: monsterKillHandler,
   // 15: createTower,
-  // 16: upgradeTower,
+  16: upgradeTower,
   21: saveTowerInfoHandler,
-
 };
 
 export default handlerMappings;
